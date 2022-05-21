6ba1dc0df0fa2805cb57a2d05fedb0cb9637e0af
Jonathan Bry <jonathanbry@Davids-MacBook-Pro.local>
2022-05-21 14:20:54 -0700

    1

--- src/App.js
+++ src/App.js
@@ -749,7 +749,7 @@ function App() {
               }}
             >
               TWO WAYS TO MINT <br />
-              <span style={{ fontSize: "27px" }}>{data.totalSupply || 1344} / {CONFIG.MAX_SUPPLY}</span>
+              <span style={{ fontSize: "27px" }}>{data.totalSupply || 1532 } / {CONFIG.MAX_SUPPLY}</span>
             </s.TextTitle>
             <s.TextDescription
               style={{
@@ -1019,7 +1019,7 @@ function App() {
               SUN & MOON
               GENESIS NFT
             </h1>
-            <span style={{ fontSize: "27px", color: "rgb(45, 236, 182)", fontWeight: "700" }}>{data.totalSupply || "1372"} / {CONFIG.MAX_SUPPLY}</span>
+            <span style={{ fontSize: "27px", color: "rgb(45, 236, 182)", fontWeight: "700" }}>{data.totalSupply || "1532"} / {CONFIG.MAX_SUPPLY}</span>
           </div>
           <div>
 
